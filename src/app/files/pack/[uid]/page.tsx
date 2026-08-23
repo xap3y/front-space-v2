18ddec5f83a55b3c9194b68c7215f55faed3c9c6
panneiphyu <58666857+xap3y@users.noreply.github.com>
2026-08-23 23:12:37 +0200

    drawing

--- src/app/files/pack/[uid]/page.tsx
+++ src/app/files/pack/[uid]/page.tsx
@@ -1,9 +1,13 @@
 import {PackPageClient} from "@/app/files/pack/[uid]/client";
+import {getUserServer} from "@/app/_server/getUser";
+import AuthenticatedPageNav from "@/components/AuthenticatedPageNav";
 
 
 export default async function PackPage() {
+    const user = await getUserServer();
     return (
         <>
+            {user && <AuthenticatedPageNav />}
             <PackPageClient />
         </>
     )
